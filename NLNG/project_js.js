d24bb603b688ecfeeaaa5909ea364dcc061e33e8
Ofeoritselaju Oyan <ofeoritselaju.oyan@ashesi.edu.gh>
2021-06-15 08:39:25 +0000

    "show popup" Popup method 1

--- NLNG/project_js.js
+++ NLNG/project_js.js
@@ -1,4 +1,6 @@
 //alert ('Want to learn how to find and book speaking gigs?');
-
+function togglePopup(){
+    document.getElementById("popup-1").classList.toggle("active")
+}
 
                       
